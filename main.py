88883a4839736ae76d323fac895116080f60ee4e
n1col5 <gaby.nicole1910@gmail.com>
2026-09-09 20:14:00 -0300

    Reestructura el flujo de gestión de equipamiento en main.py para incluir un menú interactivo

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import datos
 import operaciones
 
-codigo = input ("Ingrese el código del equipo: ").strip().upper()
-tipo = input("Ingrese el tipo: Desktop, Notebook, Monitor o Celular: ").strip().capitalize()
+opcion = ""
 
-operaciones.registrar_equipo(codigo, tipo) #Valida los datos y registra el equipo si son correctos.
+while opcion != "3":
+    print("\n--- GESTIÓN DE EQUIPAMIENTO ---")
+    print("1. Registrar equipo")
+    print("2. Listar equipos")
+    print("3. Salir")
 
-print("Códigos registrados:", datos.equipos_codigos)
-print("Tipos registrados:", datos.equipos_tipos)
-print("Estados registrados:", datos.equipos_estados)
-print("Legajos registrados:", datos.equipos_legajos)
+    opcion = input("Seleccione una opción: ").strip()
+
+    if opcion == "1":
+        codigo = input("Ingrese el código del equipo: ").strip().upper()
+
+        tipo = input("Ingrese el tipo: Desktop, Notebook, Monitor o Celular: ").strip().capitalize()
+
+        operaciones.registrar_equipo(codigo, tipo)
+
+    if opcion == "2":
+        print("\n--- LISTA DE EQUIPOS ---")
+
+        if len(datos.equipos_codigos) == 0:
+            print("Todavía no hay equipos registrados.")
+
+        else:
+            for i in range(len(datos.equipos_codigos)):
+                print(
+                    f"Codigo: {datos.equipos_codigos[i]}"
+                    f"Tipo: {datos.equipos_tipos[i]}"
+                    f"Estado: {datos.equipos_estados[i]}"
+                )
+
+    if opcion == "3":
+        print("Programa finaliado")
+
+    else:
+        print("Opción inválida. Intente nuevamente.")
 
