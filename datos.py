a83b0803c25731935f6b265bdea611c669044e73
Mateo Gaspar Benites <benitesmateogaspar@gmail.com>
2026-09-09 13:50:36 -0300

    Agrega tuplas, listas y matriz inicial en datos.py

---
+++ datos.py
@@ -0,0 +1,23 @@
+# Tuplas para datos fijos 
+estados_equipos = ("Disponible", "Asignado", "En reparacion", "Fuera de servicio")
+departamentos = ("Administración", "Ventas", "Sistemas", "Marketing")
+tipos_equipos = ("Desktop", "Notebook", "Monitor", "Celular")
+
+# Listas paralelas - Estructuca para empleados.
+empleados_legajos = []
+empleados_nombres = []
+empleados_departamentos = []
+
+# Listas paralelas - Estructura para equipos. 
+equipos_codigos = []
+equipos_tipos  = []
+equipos_estados = []
+equipos_legajos = []
+
+# Matriz - Departamentos x Tipos, inicializada en 0
+matriz_asignaciones = []
+for i in departamentos:          # por cada departamento (4 veces)
+    fila = []
+    for j in tipos_equipos:        # por cada tipo de equipo (4 veces)
+        fila.append(0)
+    matriz_asignaciones.append(fila)
