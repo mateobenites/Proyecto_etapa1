83b49044afc794f3f4ecc34a792cb6509846cc46
n1col5 <gaby.nicole1910@gmail.com>
2026-09-09 19:57:35 -0300

    Agrega impresión de datos registrados en main.py

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import operaciones
 codigo = input ("Ingrese el código del equipo: ").strip().upper()
 tipo = input ("Ingrese el tipo: Desktop, Notebook, Monitos o Celular ").strip()
 
-operaciones.registrar_equipo(codigo, tipo)
+operaciones.registrar_equipo(codigo, tipo) #Valida los datos y registra el equipo si son correctos.
+
+print("Códigos registrados:", datos.equipos_codigos)
+print("Tipos registrados:", datos.equipos_tipos)
+print("Estados registrados:", datos.equipos_estados)
+print("Legajos registrados:", datos.equipos_legajos)
 
